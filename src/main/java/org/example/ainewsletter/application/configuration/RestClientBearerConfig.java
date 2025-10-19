3fb75e94f898bb41c07ec8f03f87d387ea44b50c
alexandre <rodenas.alexandre@gmail.com>
2025-10-19 15:06:45 +0200

    Ollama cloud

---
+++ src/main/java/org/example/ainewsletter/application/configuration/RestClientBearerConfig.java
@@ -0,0 +1,20 @@
+package org.example.ainewsletter.application.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientBearerConfig {
+
+
+    @Bean
+    public RestClient.Builder restClientBuilder(
+        @Value("${spring.ai.ollama.api-key}") String token
+    ) {
+        return RestClient.builder()
+            .defaultHeader("Authorization", "Bearer " + token);
+    }
+
+}
